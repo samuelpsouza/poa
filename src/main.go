f1892579a0242d09b172d47a59093b0edf3405b6
Samuel Souza <ssouza.himself@gmail.com>
2024-05-11 18:56:50 -0300

    using env to get port

--- src/main.go
+++ src/main.go
@@ -1,15 +1,27 @@
 package main
 
 import (
+	"os"
 	"net/http"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
+func init() {
+	godotenv.Load()
+}
+
 func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "8080"
+	}
+
 	router := gin.Default()
 	router.GET("/hello", greeting)
 
-	router.Run("localhost:8080")
+	router.Run("localhost:" + port)
 }
 
 func greeting(c *gin.Context) {
